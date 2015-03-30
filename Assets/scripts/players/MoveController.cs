24fb008adc0874560e7ab1abda63c3776c6222e0
Julien Berhou <julien@grifdail.fr>
2015-03-30 18:21:10 +0200

    First commit init move

---
+++ Assets/scripts/players/MoveController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+	public float playerSpeed = 1;
+	public float playerFriction = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
