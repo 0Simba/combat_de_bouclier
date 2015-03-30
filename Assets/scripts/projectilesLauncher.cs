561789c87489ba85614044fc556049b1d2c01463
0Simba <b.dersoir@student.isartdigital.com>
2015-03-30 20:56:47 +0200

    Can instantiate projectile with key a

---
+++ Assets/scripts/projectilesLauncher.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectilesLauncher : MonoBehaviour {
+
+    public string     keyName = "a";
+    public GameObject GOprojectile;
+
+	void Start () {
+
+	}
+
+	void Update () {
+        if (Input.GetKeyDown(keyName) && GOprojectile) {
+            GameObject a = Instantiate(GOprojectile, transform.position, Quaternion.identity) as GameObject;
+        }
+ 	}
+}
