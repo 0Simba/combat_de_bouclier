399dacfebfc3c333d7ca468407195b1c19c848ff
0Simba <b.dersoir@student.isartdigital.com>
2015-03-30 22:13:06 +0200

    Set projectiles collision stop velocity and set projectiles pickable

--- Assets/scripts/projectile.cs
+++ Assets/scripts/projectile.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
-    /*==========  REAL  ==========*/
-
-    public  int         force = 5000;
+    public  int         force    = 5000;
+    private bool        pickable = false;
     private Rigidbody2D rigidbody;
-
 
     public void MyInit (Vector2 direction) {
         rigidbody = GetComponent<Rigidbody2D>();
-        transform.Translate(direction);        // TODO be more safe
+        transform.Translate(direction * 2);        // TODO be more safe
         rigidbody.AddForce(direction * force);
+    }
+
+    void OnCollisionEnter2D (Collision2D col) {
+        pickable = true;
+        rigidbody.velocity = Vector2.zero;
     }
 
 	void Start () {
