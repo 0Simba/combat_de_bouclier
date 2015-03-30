36d0f95e193468ed1f4b0765c3361235f67caa47
0Simba <b.dersoir@student.isartdigital.com>
2015-03-30 20:35:36 +0200

    Prepare projectile

---
+++ Assets/scripts/projectile.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectile : MonoBehaviour {
+
+    /*==========  TEMPS TEST  ==========*/
+    private bool beInit = false;
+
+    /*==========  REAL  ==========*/
+
+    public  int         force = 5000;
+    private Vector2     direction;
+    private Rigidbody2D rigidbody;
+
+
+    void MyInit (Vector2 _direction) {
+        direction = _direction;
+    }
+
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();;
+	}
+
+    void Update() {
+        if (Input.GetKeyDown("space")) {
+            rigidbody.AddForce(Vector2.up * force);
+        }
+
+        if (beInit) {
+        }
+    }
+}
