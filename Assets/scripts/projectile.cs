ecfa60054e0019e2c58959871a17dbcf8e597b1a
0Simba <b.dersoir@student.isartdigital.com>
2015-03-30 23:08:31 +0200

    Add collisions beetwen projectiles

--- Assets/scripts/projectile.cs
+++ Assets/scripts/projectile.cs
@@ -6,22 +6,49 @@ public class Projectile : MonoBehaviour {
     public  int         force    = 5000;
     private bool        pickable = false;
     private Rigidbody2D rigidbody;
+    private Vector2     lastVelocity;
+
+    void Awake () {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update () {
+        lastVelocity = rigidbody.velocity;
+    }
 
     public void MyInit (Vector2 direction) {
-        rigidbody = GetComponent<Rigidbody2D>();
         transform.Translate(direction * 2);        // TODO be more safe
         rigidbody.AddForce(direction * force);
     }
 
     void OnCollisionEnter2D (Collision2D col) {
+        string layerName = LayerMask.LayerToName(col.gameObject.layer);
+
+        if      (layerName == "Walls")       WallsCollision();
+        else if (layerName == "Players")     PlayersCollision();
+        else if (layerName == "Projectiles") ProjectileCollision();
+        else if (layerName != "Default")     Debug.Log("Pas de collision entre les projectiles et le layer : " + layerName);
+    }
+
+    void WallsCollision () {
         pickable = true;
         rigidbody.velocity = Vector2.zero;
     }
 
-	void Start () {
-	}
+    void ProjectileCollision () {
+        Debug.Log("dans projectile collision");
+        pickable = true;
+        rigidbody.velocity = Vector2.zero;
+    }
 
-    void Update() {
-
+    void PlayersCollision () {
+        if (pickable) {
+            Debug.Log("Coder le ramassage de projectile");
+        }
+        else {
+            Debug.Log("Coder la perte de vie du joueur");
+            rigidbody.AddForce(lastVelocity * -10);         // TODO use var
+            pickable = true;
+        }
     }
 }
