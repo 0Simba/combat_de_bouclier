c43b173f6c55b9e700782b9f9eec89911ff3cbf6
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2015-03-31 15:45:41 +0200

    Menu de manette à la towerfall

--- Assets/scripts/DeviceManager.cs
+++ Assets/scripts/DeviceManager.cs
@@ -6,14 +6,30 @@ public class DeviceManager : MonoBehaviour {
 
 	[HideInInspector]
 	public static InputDevice currentDevice;
+	private InControlManager inControlScript;
 
 	// Use this for initialization
 	void Start () {
 		currentDevice = InputManager.ActiveDevice;
+		inControlScript = GetComponent<InControlManager> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		currentDevice = InputManager.ActiveDevice;
+		if (InputManager.Devices.Count < 4)
+		{
+			if (inControlScript.customProfiles.Count == 0)
+			{
+				inControlScript.customProfiles.Insert(0, "ComputerProfile.KeyboardAndMouseProfile");
+			}
+		}
+		else if (InputManager.Devices.Count > 4)
+		{
+			if (inControlScript.customProfiles[0] != null)
+			{
+				inControlScript.customProfiles.Remove("0");
+			}
+		}
 	}
 }
---
+++ Assets/scripts/DeviceUI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using InControl;
+using UnityEngine.UI;
+
+public class DeviceUI : MonoBehaviour {
+
+	public int playerSlot;
+	private bool isOk;
+
+	void Update()
+	{
+		var inputDevice = (InputManager.Devices.Count > playerSlot) ? InputManager.Devices[playerSlot] : null;
+		if (inputDevice == null)
+		{
+			// If no controller exists for this cube, just make it translucent.
+			GetComponent<Image>().color = new Color ( 1.0f, 1.0f, 1.0f, 0.2f);
+			GetComponentInChildren<Text>().text = "No Controller";
+			isOk = false;
+		}
+		else if (InputManager.Devices[playerSlot] != null)
+		{
+			if (isOk == false)
+			{
+				GetComponent<Image>().color = new Color ( 1.0f, 1.0f, 1.0f, 0.6f);
+				GetComponentInChildren<Text>().text = inputDevice.Name;
+				if (InputManager.Devices[playerSlot].Action1.WasPressed)
+				{
+					isOk = true;
+				}
+			}
+			if (isOk == true)
+			{
+				GetComponent<Image>().color = new Color ( 1.0f, 1.0f, 1.0f, 1.0f);
+				GetComponentInChildren<Text>().text = "OK !";
+				if (InputManager.Devices[playerSlot].Action2.WasPressed)
+				{
+					isOk = false;
+				}
+			}
+		}
+	}
+}
