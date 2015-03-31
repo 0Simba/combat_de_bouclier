db5418f0450ae21c7ac861c95221637a8336bb28
0Simba <b.dersoir@student.isartdigital.com>
2015-03-31 14:32:31 +0200

    Display aim

--- Assets/scripts/players/Aim.cs
+++ Assets/scripts/players/Aim.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class Aim : MonoBehaviour {
 
-    public Vector2 direction = Vector2.right;
-
+    public Vector2    direction = Vector2.right;
+    public GameObject aimSprite;
 
     void Start () {
     }
 
 
 	void Update () {
-        direction = new Vector2(DeviceManager.currentDevice.RightStickX, DeviceManager.currentDevice.RightStickY);
+        direction            = new Vector2(DeviceManager.currentDevice.RightStickX, DeviceManager.currentDevice.RightStickY);
+        float angle          = Mathf.Atan2(direction.x, direction.y) / Mathf.PI * -180;
+        Quaternion rotation  = Quaternion.identity;
+        rotation.eulerAngles = new Vector3(0, 0, angle);
+
+        aimSprite.transform.rotation = rotation;
     }
 }
