4ea24a53836e89ed6c2b45d9c5e283207e7bb17c
0Simba <b.dersoir@student.isartdigital.com>
2015-04-01 20:46:45 +0200

    Prepapre sounds

---
+++ Assets/scripts/polish/Sounds.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sounds : MonoBehaviour {
+
+    static public AudioSource hit;
+    static public AudioSource pick;
+    static public AudioSource cast;
+    static public AudioSource kill;
+
+    public AudioSource _hit;
+    public AudioSource _pick;
+    public AudioSource _cast;
+    public AudioSource _kill;
+
+	void Start () {
+        hit = _hit;
+        pick = _pick;
+        cast = _cast;
+        kill = _kill;
+	}
+
+	void Update () {
+	}
+}
