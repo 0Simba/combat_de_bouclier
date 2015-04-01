be710b20c7671c73c35b089e092c61b23a1355df
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2015-04-01 12:08:12 +0200

    Init de la scène de jeu avec le bon nombre de joueurs

--- Assets/scripts/MainGame.cs
+++ Assets/scripts/MainGame.cs
@@ -10,10 +10,18 @@ public class MainGame : MonoBehaviour {
     static int respawnDuration;
     public int _respawnDuration;
 
-
+	public MainPlayer[] players;
 
     void Start () {
         playersScores   = _playersScores;
         respawnDuration = _respawnDuration;
+
+		for(int i = 0; i < players.Length; i++)
+		{
+			if (GameDatas.playersInputIndex[i] != -1)
+			{
+				players[i].transform.gameObject.SetActive(true);
+			}
+		}
     }
 }
