b08d8a80d737e2e936c01e369a057615e4623f0e
Julien Berhou <julien@grifdail.fr>
2015-04-02 15:00:14 +0200

    Looping map

---
+++ Assets/scripts/LoopMap.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopMap : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(LayerMask.LayerToName(col.gameObject.layer));
+        col.transform.position = new Vector3(col.transform.position.x ,- col.transform.position.y, col.transform.position.z);
+    }
+}
