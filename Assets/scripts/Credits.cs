ffefb312bef0994fdaacc584c2cf1cab30b24fdb
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2015-04-02 11:00:32 +0200

    Credits

---
+++ Assets/scripts/Credits.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour {
+
+	public float speed;
+	public float maxX;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponentInChildren<Text> ().transform.position -= new Vector3(speed, 0f, 0f);
+		if (GetComponentInChildren<Text> ().transform.localPosition.x < -maxX)
+		{
+			GetComponentInChildren<Text> ().transform.localPosition = new Vector3(0, 0, 0);
+		}
+		if (GetComponentInChildren<Text> ().transform.localPosition.x > 0)
+		{
+			GetComponentInChildren<Text> ().transform.localPosition = new Vector3(0, 0, 0);
+		}
+	}
+}
