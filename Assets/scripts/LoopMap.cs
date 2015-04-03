d1a6ad49b35f823955342dbc7b576896bb45bf0f
Julien Berhou <julien@grifdail.fr>
2015-04-03 09:47:42 +0200

    Commit is what i need !

--- Assets/scripts/LoopMap.cs
+++ Assets/scripts/LoopMap.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class LoopMap : MonoBehaviour {
 
+    public float add = 0;
+
     void OnTriggerEnter2D(Collider2D col)
     {
-        col.transform.position = new Vector3(col.transform.position.x ,- col.transform.position.y, col.transform.position.z);
+        col.transform.position = new Vector3(col.transform.position.x ,- col.transform.position.y + add, col.transform.position.z);
     }
 }
